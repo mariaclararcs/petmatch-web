efd6fdfd8a367b4682212bafe93c43a3747f921b
mariaclararcs <mariaclarar981@gmail.com>
2025-05-23 15:56:23 -0300

    feat(pages): add pages folders and files

---
+++ src/app/animals/page.tsx
@@ -0,0 +1,11 @@
+/* Página contendo:
+- Seção de filtragem por:
+    - Ordenação alfabética dos nomes (A-Z, Z-A)
+    - Tipo (todos, cachorros, gatos, outros)
+    - Tempo no abrigo (mais tempo, menos tempo)
+    - Estado
+    - Cidade
+    - Sexo (fêmea, macho)
+    - Idade (mais velhos, mais novos)
+- Listagem de cards de animais
+*/
---
+++ src/app/ongs/page.tsx
@@ -0,0 +1,8 @@
+/* Página contendo:
+- Seção de filtragem por:
+    - Ordenação alfabética dos nomes (A-Z, Z-A)
+    - Tipo de animais que abriga (cachorros, gatos, outros)
+    - Estado
+    - Cidade
+- Listagem de cards de ongs
+*/
